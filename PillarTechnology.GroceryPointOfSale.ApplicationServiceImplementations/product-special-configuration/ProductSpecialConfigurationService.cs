80aae6256f43710dc2cf8c0dad82a9fb336daa99
Jarrod Swords <jarrod.swords@gmail.com>
2018-11-07 18:36:42 -0500

    Refactor create special

--- PillarTechnology.GroceryPointOfSale.ApplicationServiceImplementations/product-special-configuration/ProductSpecialConfigurationService.cs
+++ PillarTechnology.GroceryPointOfSale.ApplicationServiceImplementations/product-special-configuration/ProductSpecialConfigurationService.cs
@@ -1,3 +1,4 @@
+using System;
 using AutoMapper;
 using PillarTechnology.GroceryPointOfSale.ApplicationServices;
 using PillarTechnology.GroceryPointOfSale.Domain;
@@ -17,21 +18,27 @@ namespace PillarTechnology.GroceryPointOfSale.ApplicationServiceImplementations
 
         public ProductDto CreateBuyNForXAmountSpecial(CreateBuyNForXAmountSpecialArgs args)
         {
-            var product = _productRepository.FindProduct(args.ProductName);
-            product.Special = new BuyNForXAmountSpecial(args.StartTime, args.EndTime, args.DiscountedItems.Value, args.GroupSalePrice.Value, args.Limit);
-            var persistedProduct = _productRepository.UpdateProduct(product);
-            var productDto = _mapper.Map<ProductDto>(persistedProduct);
-            productDto.Special = _mapper.Map<BuyNForXAmountSpecialDto>(persistedProduct.Special);
-            return productDto;
+            Func<Special> createSpecial = () => new BuyNForXAmountSpecial(args.StartTime, args.EndTime, args.DiscountedItems.Value, args.GroupSalePrice.Value, args.Limit);
+            Func<Special, ISpecialDto> mapToSpecialDto = special => _mapper.Map<BuyNForXAmountSpecialDto>(special);
+            return CreateSpecial(args, createSpecial, mapToSpecialDto);
         }
 
         public ProductDto CreateBuyNGetMAtXPercentOffSpecial(CreateBuyNGetMAtXPercentOffSpecialArgs args)
         {
+            Func<Special> createSpecial = () => new BuyNGetMAtXPercentOffSpecial(args.StartTime, args.EndTime, args.PreDiscountItems.Value, args.DiscountedItems.Value, args.PercentageOff.Value, args.Limit);
+            Func<Special, ISpecialDto> mapToSpecialDto = special => _mapper.Map<BuyNGetMAtXPercentOffSpecialDto>(special);
+            return CreateSpecial(args, createSpecial, mapToSpecialDto);
+        }
+
+        private ProductDto CreateSpecial(CreateSpecialArgs args, Func<Special> createSpecial, Func<Special, ISpecialDto> mapToSpecialDto)
+        {
             var product = _productRepository.FindProduct(args.ProductName);
-            product.Special = new BuyNGetMAtXPercentOffSpecial(args.StartTime, args.EndTime, args.PreDiscountItems.Value, args.DiscountedItems.Value, args.PercentageOff.Value, args.Limit);
+
+            product.Special = createSpecial();
             var persistedProduct = _productRepository.UpdateProduct(product);
+
             var productDto = _mapper.Map<ProductDto>(persistedProduct);
-            productDto.Special = _mapper.Map<BuyNGetMAtXPercentOffSpecialDto>(persistedProduct.Special);
+            productDto.Special = mapToSpecialDto(product.Special);
             return productDto;
         }
     }
