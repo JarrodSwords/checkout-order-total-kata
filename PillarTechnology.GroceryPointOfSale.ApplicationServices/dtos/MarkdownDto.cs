b5c2d160e8782fec7be339ae0c0dfe3fe2c1e2ec
Jarrod Swords <jarrod.swords@gmail.com>
2018-11-10 22:33:59 -0500

    Add missing is active property to markdown dto

--- PillarTechnology.GroceryPointOfSale.ApplicationServices/dtos/MarkdownDto.cs
+++ PillarTechnology.GroceryPointOfSale.ApplicationServices/dtos/MarkdownDto.cs
@@ -5,7 +5,8 @@ namespace PillarTechnology.GroceryPointOfSale.ApplicationServices
     public class MarkdownDto
     {
         public decimal AmountOffRetail { get; set; }
-        public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
+        bool IsActive { get; set; }
+        public DateTime StartTime { get; set; }
     }
 }
