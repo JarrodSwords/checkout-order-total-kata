1d59e94301cc328246fb0e8be6b112e07859465d
Jarrod Swords <jarrod.swords@gmail.com>
2018-11-21 04:23:48 -0500

    Create line item factory subfolder

---
+++ Domain/models/order/scanned-items/line-item-factory/EachesLineItemFactory.cs
@@ -0,0 +1,16 @@
+using NodaMoney;
+using UnitsNet;
+
+namespace PointOfSale.Domain
+{
+    public class EachesLineItemFactory : LineItemFactory
+    {
+        public override Mass Mass => throw new System.NotImplementedException();
+        public override Money SalePrice => Product.RetailPrice;
+
+        public EachesLineItemFactory(Product product) : base(product) { }
+
+        public override MarkdownLineItem CreateMarkdownLineItem() =>
+            new MarkdownLineItem(Product.Name, -Product.Markdown.AmountOffRetail, Id);
+    }
+}
